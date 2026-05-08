c52a3019b30a99b4851ddd230827f64cbaec49a4
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2026-05-08 21:16:51 +0330

    add JWT utility

---
+++ utils/jwt.go
@@ -0,0 +1,69 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/saleh-ghazimoradi/GopherMarket/config"
+	"time"
+)
+
+type Claims struct {
+	UserId uint   `json:"user_id"`
+	Email  string `json:"email"`
+	Role   string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(cfg *config.Config, userId uint, email, role string) (accessToken, refreshToken string, err error) {
+	accessClaims := &Claims{
+		UserId: userId,
+		Email:  email,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(cfg.JWT.ExpiresIn)),
+			Issuer:    fmt.Sprint(userId),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, accessClaims)
+	accessTokenString, err := at.SignedString([]byte(cfg.JWT.Secret))
+	if err != nil {
+		return "", "", err
+	}
+
+	refreshClaims := &Claims{
+		UserId: userId,
+		Email:  email,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(cfg.JWT.ExpiresIn)),
+			Issuer:    fmt.Sprint(userId),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	rt := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims)
+	refreshTokenString, err := rt.SignedString([]byte(cfg.JWT.Secret))
+	if err != nil {
+		return "", "", err
+	}
+
+	return accessTokenString, refreshTokenString, nil
+}
+
+func ValidateToken(tokenString, secret string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (any, error) {
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, errors.New("invalid token")
+}
