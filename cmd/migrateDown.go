6864566d68c2f05b3a1e8fe99a393068f6d03675
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2026-05-08 16:36:09 +0330

    implement the migrate up and down

---
+++ cmd/migrateDown.go
@@ -0,0 +1,73 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/saleh-ghazimoradi/GopherMarket/config"
+	"github.com/saleh-ghazimoradi/GopherMarket/infra/migration"
+	"github.com/saleh-ghazimoradi/GopherMarket/infra/postgresql"
+	"github.com/saleh-ghazimoradi/GopherMarket/internal/logger"
+
+	"github.com/spf13/cobra"
+)
+
+// migrateDownCmd represents the migrateDown command
+var migrateDownCmd = &cobra.Command{
+	Use:   "migrateDown",
+	Short: "A brief description of your command",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("migrateDown called")
+
+		sLogger := logger.NewSlogLogger()
+
+		cfg, err := config.GetConfigInstance()
+		if err != nil {
+			sLogger.Error("failed to load config", "error", err)
+			return
+		}
+
+		pgCfg := postgresql.NewPostgresql(
+			postgresql.WithHost(cfg.Postgresql.Host),
+			postgresql.WithPort(cfg.Postgresql.Port),
+			postgresql.WithUser(cfg.Postgresql.User),
+			postgresql.WithPassword(cfg.Postgresql.Password),
+			postgresql.WithName(cfg.Postgresql.Name),
+			postgresql.WithMaxOpenConn(cfg.Postgresql.MaxOpenConn),
+			postgresql.WithMaxIdleConn(cfg.Postgresql.MaxIdleConn),
+			postgresql.WithMaxIdleTime(cfg.Postgresql.MaxIdleTime),
+			postgresql.WithSSLMode(cfg.Postgresql.SSLMode),
+			postgresql.WithTimeout(cfg.Postgresql.Timeout),
+			postgresql.WithLogger(sLogger),
+		)
+
+		db, err := pgCfg.Connect()
+		if err != nil {
+			sLogger.Error("failed to connect database", "error", err)
+			return
+		}
+
+		sqlDB, err := db.DB()
+		if err != nil {
+			sLogger.Error("failed to get sql.DB from gorm", "error", err)
+			return
+		}
+		defer sqlDB.Close()
+
+		migrator, err := migration.NewMigrator(sqlDB, cfg.Postgresql.Name)
+		if err != nil {
+			sLogger.Error("migration init failed", "error", err)
+			return
+		}
+		defer migrator.Close()
+
+		if err := migrator.Down(); err != nil {
+			sLogger.Error("rollback failed", "error", err)
+			return
+		}
+
+		sLogger.Info("Last migration rolled back successfully")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(migrateDownCmd)
+}
