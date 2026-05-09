c9f55fd71d263b1055ff70cb29e4accc01ea53f7
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2026-05-09 18:31:09 +0330

    update domains

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -29,14 +29,3 @@ type User struct {
 	Orders        []Order
 	Cart          Cart
 }
-
-type RefreshToken struct {
-	Id        uint      `gorm:"primaryKey"`
-	UserId    uint      `gorm:"not null"`
-	Token     string    `gorm:"uniqueIndex;not null"`
-	ExpiresAt time.Time `gorm:"not null"`
-	CreatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
-
-	User User
-}
