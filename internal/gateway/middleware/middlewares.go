80df5995dd5055470885a72e490f0070981486ea
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2026-05-09 11:29:41 +0330

    add authentication and admin middlewares

--- internal/gateway/middleware/middlewares.go
+++ internal/gateway/middleware/middlewares.go
@@ -2,13 +2,18 @@ package middleware
 
 import (
 	"fmt"
+	"github.com/saleh-ghazimoradi/GopherMarket/config"
+	"github.com/saleh-ghazimoradi/GopherMarket/internal/domain"
 	"github.com/saleh-ghazimoradi/GopherMarket/internal/helper"
+	"github.com/saleh-ghazimoradi/GopherMarket/utils"
 	"log/slog"
 	"net/http"
+	"strings"
 )
 
 type Middleware struct {
 	logger *slog.Logger
+	cfg    *config.Config
 }
 
 func (m *Middleware) Logging(next http.Handler) http.Handler {
@@ -40,6 +45,53 @@ func (m *Middleware) Recover(next http.Handler) http.Handler {
 				helper.InternalServerError(w, "panic recovery hit", fmt.Errorf("%v", err))
 			}
 		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (m *Middleware) Authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authorization := r.Header.Get("Authorization")
+		if authorization == "" {
+			helper.UnauthorizedResponse(w, "Authorization header missing")
+			return
+		}
+
+		tokenParts := strings.Split(authorization, " ")
+		if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
+			helper.UnauthorizedResponse(w, "Invalid authorization header format")
+			return
+		}
+
+		claims, err := utils.ValidateToken(tokenParts[1], m.cfg.JWT.Secret)
+		if err != nil {
+			helper.UnauthorizedResponse(w, "Unauthorized")
+			return
+		}
+
+		ctx := r.Context()
+		ctx = utils.WithUserId(ctx, claims.UserId)
+		ctx = utils.WithEmailKey(ctx, claims.Email)
+		ctx = utils.WithRoleKey(ctx, claims.Role)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r)
+
+	})
+}
+
+func (m *Middleware) Admin(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		role, exists := utils.RoleFromContext(r.Context())
+		if !exists {
+			helper.ForbiddenResponse(w, "Access Denied")
+			return
+		}
+
+		if role != string(domain.Admin) {
+			helper.ForbiddenResponse(w, "Access Denied")
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
