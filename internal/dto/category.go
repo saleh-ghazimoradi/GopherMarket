8e03360228e465b38d114e6a879b3668c4caaadf
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2026-05-10 14:11:40 +0330

    improve updateCategoryRequest dto

--- internal/dto/category.go
+++ internal/dto/category.go
@@ -8,9 +8,9 @@ type CreateCategoryRequest struct {
 }
 
 type UpdateCategoryRequest struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	IsActive    *bool  `json:"is_active"`
+	Name        *string `json:"name"`
+	Description *string `json:"description"`
+	IsActive    *bool   `json:"is_active"`
 }
 
 type CategoryResponse struct {
