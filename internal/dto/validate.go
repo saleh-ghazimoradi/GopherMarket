505af6f63608c3b15c96de226404491b41a8c44c
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2026-05-10 19:31:24 +0330

    add some dtos validators

--- internal/dto/validate.go
+++ internal/dto/validate.go
@@ -23,6 +23,30 @@ func validateRefreshToken(v *helper.Validator, refreshToken string) {
 	v.Check(helper.NotBlank(refreshToken), "refresh_token", "Refresh token must be provided")
 }
 
+func validateCategoryName(v *helper.Validator, name string) {
+	v.Check(helper.NotBlank(name), "name", "Category name must be provided")
+}
+
+func validateCategoryId(v *helper.Validator, id uint) {
+	v.Check(id > 0, "id", "Category ID must be greater than 0")
+}
+
+func validateProductName(v *helper.Validator, name string) {
+	v.Check(helper.NotBlank(name), "name", "Product name must be provided")
+}
+
+func validateProductPrice(v *helper.Validator, price float64) {
+	v.Check(price > 0, "price", "Price must be greater than 0")
+}
+
+func validateStock(v *helper.Validator, stock int) {
+	v.Check(stock > 0, "stock", "Stock must be greater than 0")
+}
+
+func validateProductSKU(v *helper.Validator, sku string) {
+	v.Check(helper.NotBlank(sku), "sku", "Product sku must be provided")
+}
+
 func ValidateRegisterRequest(v *helper.Validator, req *RegisterRequest) {
 	validateFirstNameAndLastName(v, req.FirstName)
 	validateFirstNameAndLastName(v, req.LastName)
@@ -43,7 +67,44 @@ func ValidateLogoutRequest(v *helper.Validator, req *LogoutRequest) {
 	validateRefreshToken(v, req.RefreshToken)
 }
 
-func ValidateProfileRequest(v *helper.Validator, req *UpdateProfileRequest) {
-	validateFirstNameAndLastName(v, *req.FirstName)
-	validateFirstNameAndLastName(v, *req.LastName)
+func ValidateUpdateProfileRequest(v *helper.Validator, req *UpdateProfileRequest) {
+	if req.FirstName != nil {
+		validateFirstNameAndLastName(v, *req.FirstName)
+	}
+	if req.LastName != nil {
+		validateFirstNameAndLastName(v, *req.LastName)
+	}
+}
+
+func ValidateCreateCategoryRequest(v *helper.Validator, req *CreateCategoryRequest) {
+	validateCategoryName(v, req.Name)
+}
+
+func ValidateUpdateCategoryRequest(v *helper.Validator, req *UpdateCategoryRequest) {
+	if req.Name != nil {
+		validateCategoryName(v, *req.Name)
+	}
+}
+
+func ValidateCreateProductRequest(v *helper.Validator, req *CreateProductRequest) {
+	validateCategoryId(v, req.CategoryId)
+	validateProductName(v, req.Name)
+	validateProductPrice(v, req.Price)
+	validateStock(v, req.Stock)
+	validateProductSKU(v, req.SKU)
+}
+
+func ValidateUpdateProductRequest(v *helper.Validator, req *UpdateProductRequest) {
+	if req.Name != nil {
+		validateProductName(v, *req.Name)
+	}
+	if req.CategoryId != nil {
+		validateCategoryId(v, *req.CategoryId)
+	}
+	if req.Price != nil {
+		validateProductPrice(v, *req.Price)
+	}
+	if req.Stock != nil {
+		validateStock(v, *req.Stock)
+	}
 }
