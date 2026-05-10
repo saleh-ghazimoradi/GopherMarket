ff290b91b1f38158d899330aac29157f7b8be174
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2026-05-10 13:56:43 +0330

    implement the redis cache repository

---
+++ internal/repository/cache.go
@@ -0,0 +1,55 @@
+package repository
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/redis/go-redis/v9"
+	"time"
+)
+
+type RedisCache interface {
+	Get(ctx context.Context, key string, dest any) error
+	Set(ctx context.Context, key string, value any, ttl time.Duration) error
+	Delete(ctx context.Context, key string) error
+	DeletePattern(ctx context.Context, pattern string) error
+}
+
+type redisCache struct {
+	client *redis.Client
+}
+
+func (r *redisCache) Get(ctx context.Context, key string, dest any) error {
+	data, err := r.client.Get(ctx, key).Bytes()
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(data, dest)
+}
+
+func (r *redisCache) Set(ctx context.Context, key string, value any, ttl time.Duration) error {
+	data, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	return r.client.Set(ctx, key, data, ttl).Err()
+}
+
+func (r *redisCache) Delete(ctx context.Context, key string) error {
+	return r.client.Del(ctx, key).Err()
+}
+
+func (r *redisCache) DeletePattern(ctx context.Context, pattern string) error {
+	iter := r.client.Scan(ctx, 0, pattern, 0).Iterator()
+	for iter.Next(ctx) {
+		if err := r.client.Del(ctx, iter.Val()).Err(); err != nil {
+			return err
+		}
+	}
+	return iter.Err()
+}
+
+func NewRedisCache(client *redis.Client) RedisCache {
+	return &redisCache{
+		client: client,
+	}
+}
